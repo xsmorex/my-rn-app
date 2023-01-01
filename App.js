dac23fd8d51b6de6012ab2f3e7db9c565955c7d2
xsmorex <banasikannie@gmail.com>
2023-01-01 23:54:13 +0000

    styling the layout wip

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>lol</Text> 
-      <Button title="LOL"/>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        {/* input, add text and a button */}
+        <TextInput style={styles.textInput} placeholder="Your course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        {/*the list of goals that are renderred */}
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  appContainer:{
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput:{
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    marginRight: 8,
+    padding: 8,
+
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    flex: 0,
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 10,
   },
 });
